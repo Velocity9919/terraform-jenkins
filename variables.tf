d52d34ce8ffabc58187ee1a5f1bac90bfd68844e
Aseemakram19 <mohdaseemakram19@gmail.com>
2024-02-02 23:16:08 +0530

    added value

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "instance_name" {
-    default = "Jenkins Server"  # Names the instance
+    default = "Jenkins Server"  # Names of the instance
 }
 
 variable "key_name" {
-  default = "YOUR_KEY_NAME"                  # Names of key in aws
+  default = "jenkins tf key"                  # Names of key in aws
 }
 variable "private_key" {
-  default = "YOUR_PRIVATE_KEY_PEM_FILE_PATH"      # file path of private pem key
+  default = "."      # file path of private pem key
 }
 
 variable "access_key" {
-  default = "AWS_ACCESS_KEY"                # aws access key
+  default = "AHYoOLGiLW+oRvvG3c7nqHRgohyRY9WzTpm91SkP"                # aws access key
 }
 
 variable "secret_key" {
-  default = "AWS_SECRET_KEY"         # aws secret key
+  default = "AHYoOLGiLW+oRvvG3c7nqHRgohyRY9WzTpm91SkP"         # aws secret key
 }
 
